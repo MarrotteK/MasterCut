5e8ac6d516cf0472ac9028b25bebe4e7c70400c6
MarrotteK <kyle.marrotte@gmail.com>
2016-11-11 00:38:59 -0500

    Arduino POC comments
    Added comments to the proof-of-concept sketch that I shoulda put in
    before

--- arduino_code/mastercutsketch/mastercutsketch.ino
+++ arduino_code/mastercutsketch/mastercutsketch.ino
@@ -1,28 +1,27 @@
-#include <Button.h>
+#include <Button.h>                           //Cool button library
 
-Button winButton = Button(1, INPUT);
-Button loseButton = Button(5, INPUT);
+Button winButton = Button(1, INPUT);          //"Win" Button wired to pin 1
+Button loseButton = Button(5, INPUT);         //"Lose" Button wired to pin 5
 
 void setup() {
   // put your setup code here, to run once:
-Bean.enableWakeOnConnect(true);
-Bean.enablePairingPin(true);
-pinMode(13,OUTPUT);
-BeanHid.enable();
+Bean.enableWakeOnConnect(true);               //Wakes up if something tries to connect
+Bean.enablePairingPin(true);                  //Lets device connect, pass is 000000
+BeanHid.enable();                             //Temp, Lets device act as keyboard
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool connected = Bean.getConnectionState();
+  bool connected = Bean.getConnectionState(); //Check connection
   if (connected) {
-    Bean.setLed(0, 255, 0);
-  if(winButton.isPressed()){
-    BeanHid.sendKeys("Win");
+    Bean.setLed(0, 255, 0);                   //Leaves LED on if connected
+  if(winButton.isPressed()){                  //If "Win" button is pressed,
+    BeanHid.sendKeys("Win");                  //Send the word "Win"
   }
-  if(loseButton.isPressed()){
-    BeanHid.sendKeys("Loss");
+  if(loseButton.isPressed()){                 //If "Lose" button is pressed,
+    BeanHid.sendKeys("Loss");                 //Send the word "Loss"
   }
   }
-else {Bean.setLed(0, 0, 0);
-Bean.sleep(0xFFFFFFFF);}
+else {Bean.setLed(0, 0, 0);                   //Turn LED off if not connected
+Bean.sleep(0xFFFFFFFF);}                      //Put device to sleep if not connected
   }
